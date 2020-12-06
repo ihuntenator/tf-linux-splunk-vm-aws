446ad509ce9844c6d6efdd0bbe1cc7ba2ff4989b
Ian hunt <ihuntenator@gmail.com>
2020-12-07 10:42:58 +1300

    correcting install

--- installSplunk.sh
+++ installSplunk.sh
@@ -5,10 +5,16 @@ wget="/usr/bin/wget"
 
 ${wget} -O splunk-8.1.0.1-24fd52428b5a-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.0.1&product=splunk&filename=splunk-8.1.0.1-24fd52428b5a-linux-2.6-x86_64.rpm&wget=true'
 
-sudo yum install -y splunk-8.1.0.1-24fd52428b5a-linux-2.6-x86_64.rpm
+yum install -y splunk-8.1.0.1-24fd52428b5a-linux-2.6-x86_64.rpm
 
-${SPLUNK_HOME}/bin/splunk edit user admin -password NEWPASSWORD -auth admin:changeme
-${SPLUNK_HOME}/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt
-${SPLUNK_HOME}/bin/splunk enable boot-start -user splunk
+sudo -u splunk ". /opt/splunk/bin/setSplunkEnv; /opt/splunk/bin/splunk --answer-yes --no-prompt --accept-license --seed-passwd admin"
+
+${SPLUNK_HOME}/bin/splunk enable boot-start -systemd-managed 1 -user splunk
+
+${SPLUNK_HOME}/bin/splunk start
+
+
+#${SPLUNK_HOME}/bin/splunk start --accept-license --answer-yes --auto-ports --no-prompt --seed-passwd admin
+#${SPLUNK_HOME}/bin/splunk edit user admin -password NEWPASSWORD -auth admin:changeme
 
 exit
